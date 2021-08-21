0f9bd4cc8141d596630ad609cd1e6b0f2e97157c
Ferd9 <fernando_perez01@yahoo.es>
2021-08-20 20:40:26 -0500

    funcionando correctamente

--- setup/app.js
+++ setup/app.js
@@ -1,11 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
-const color = document.querySelector("color");
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
   //se obtiene un numero aleatorio entre el 0 y 3
-  const randomNumber = 2;
+  const randomNumber = getRandomNumber(colors.length);
+  console.log(randomNumber);
+
   document.body.style.backgroundColor = colors[randomNumber];
-  color.textComponent = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber(max)
+{
+  return Math.floor(Math.random() * max);
+}
